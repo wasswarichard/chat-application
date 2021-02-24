b249bf2eadd33bb6ab26e411af215cd70057f5b4
Wasswa Richard <wasswarichard55@gmail.com>
2021-02-24 19:00:26 +0300

    chat application set up

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,7 @@
+const pool = require('../config/database');
+const url  = require('url');
+// const _ = require('underscore');
+
 const users = [];
 
 const addUser = ({id, name, room}) => {
@@ -13,6 +17,55 @@ const addUser = ({id, name, room}) => {
     return {user}
 }
 
+const postUser = async (req, res) => {
+    const Users =  await pool.query('SELECT * FROM users');
+    const url_parts =  url.parse(req.url);
+    const query = url_parts.query;
+    const search_words =  [];
+    query && query.split('&').forEach(value => search_words.push(value.split('=')[1]));
+    const user =  Users.rows.filter(row => {
+        return row.user_name === search_words[0] && row.user_room === search_words[1]
+    });
+    if(user.length > 0) {
+        return res.json({  message: `User can not be created, username  ${search_words[0]} already exits in  ${search_words[1]} room` });
+    }{
+        await pool.query('INSERT INTO users (user_name, user_room) VALUES ($1, $2)', [search_words[0], search_words[1]] );
+        return res.send({
+            username: search_words[0],
+            room : search_words[1],
+            message: `User with username  ${search_words[0]} in  ${search_words[1]} room has been created`
+        });
+
+    }
+}
+
+const getMessage = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const response = await pool.query('SELECT * FROM messages WHERE id = $1', [id]);
+    res.json(response.rows);
+};
+
+const loginUser = async (req, res) =>{
+    const Users =  await pool.query('SELECT * FROM users');
+    const url_parts =  url.parse(req.url);
+    const query = url_parts.query;
+    const search_words =  [];
+    query && query.split('&').forEach(value => search_words.push(value.split('=')[1]));
+    const user =  Users.rows.filter(row => {
+       return row.user_name === search_words[0]
+    });
+    if(user.length === 0){
+        return res.json({
+            loginSuccess: false,
+            message: "Authentication failed, username not found"
+        });
+    }else{
+        if(user[0].user_room !== search_words[1]){
+            return res.json({ loginSuccess: false, message: "Wrong chat room" });
+        }
+        return res.json({ loginSuccess: true});
+    }
+}
 const removeUser = (id) => {
     const index = users.findIndex((user) => user.id === id);
     if(index !== 1){
@@ -25,4 +78,4 @@ const getUser = (id) => users.find((user) => user.id === id);
 
 const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
-module.exports = {addUser, removeUser, getUser, getUsersInRoom}
+module.exports = {addUser, removeUser, getUser, getUsersInRoom, loginUser, postUser}
--- server/index.js
+++ server/index.js
@@ -6,14 +6,8 @@ const cors = require('cors');
 const {addUser, removeUser, getUser, getUsersInRoom } = require('./controllers/user');
 
 
-const databaseConnection = 'postgressql://postgres:root@localhost:5432/application';
-const client = new Client({
-    connectionString: databaseConnection
-})
-client.connect();
 
 
-const port = process.env.PORT || 5000;
 const router = require('./routes/router');
 
 
@@ -26,8 +20,8 @@ io.on('connection', (socket) => {
     socket.on('join', ({name, room}, callback) => {
         const {error, user} = addUser({id: socket.id, name, room});
         if (error) return callback(error);
-        socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
-        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has joined`})
+        socket.emit('message', {text: `${user.name}, welcome to the ${user.room} room `});
+        socket.broadcast.to(user.room).emit('message', {text: `${user.name}, has joined`})
         socket.join(user.room);
 
         io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
@@ -45,12 +39,12 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         const user = removeUser(socket.id);
         if(user){
-            io.to(user.room).emit('message', {user: 'admin', text: `${user.name} has left`})
+            io.to(user.room).emit('message', { text: `${user.name} has left`})
         }
     });
 });
 app.use(cors());
 app.use(router);
 
-
+const port = process.env.PORT || 5000;
 server.listen(port, ()=> console.log(`Backend Server running on http://localhost:${port}`));
