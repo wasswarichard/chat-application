c1f9a30198ef5bb59463b89ffddf8e1aa597701a
Wasswa Richard <wasswarichard55@gmail.com>
2021-02-25 14:42:51 +0300

    chat application set up

---
+++ src/components/Messages/Messages.js
@@ -0,0 +1,28 @@
+// import React from 'react';
+// import  ScrollToBottom from 'react-scroll-to-bottom';
+// import Message from "./Message/Message";
+// import './Messages.css'
+
+import React from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
+import Message from './Message/Message';
+import './Messages.css';
+
+
+// const Messages = ({messages, name}) => (
+//     <ScrollToBottom class="messages">
+//         {messages.map((message, i) => <div key={i}><Message message={message} name={name} /></div>)}
+//     </ScrollToBottom>
+//
+//
+// );
+//
+
+const Messages = ({ messages, name }) => (
+    <ScrollToBottom className="messages">
+        {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
+    </ScrollToBottom>
+);
+
+
+export default Messages;
